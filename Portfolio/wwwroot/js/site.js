89b34cd2ac1d98be8e69e596b310a21096fb6842
lego <angelofresnoza@yahoo.com.ph>
2022-09-14 18:01:06 +0800

    Updated 9/14/22 - 3

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,3 +1,7 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
     fadin('.fade', { delay: 200 })
 });
+
+//parallax
+var image = document.getElementById('main-img');
+new simpleParallax(image);
