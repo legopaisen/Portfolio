c403b06bba9397f4f579686bb28f581e2f102c4f
lego <angelofresnoza@yahoo.com.ph>
2022-10-02 23:28:08 +0800

    Updated 10/2/22

---
+++ Portfolio/Controllers/ResumeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ResumeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
