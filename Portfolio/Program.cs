4ebacccef63e2de43d3e85ef4cb5151505d1729d
lego <angelofresnoza@yahoo.com.ph>
2022-10-02 23:47:23 +0800

    Changed landing page

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Landing}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
